d2019c2ece5cdbaac63ea098a0d7a7ceafd21891
Karan Dulloo <59745494+karandulloo@users.noreply.github.com>
2024-09-29 14:17:19 +0200

    Add files via upload

---
+++ src/main/java/com/karan/project/peopleCart/PeopleCartApplication.java
@@ -0,0 +1,13 @@
+package com.karan.project.peopleCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karan/project/peopleCart/PeopleCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karan.project.peopleCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
